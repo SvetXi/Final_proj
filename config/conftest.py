348dd9e0a18192008d88d8714f48b623a2c48982
SvetXi <svetamisaleva@mail.ru>
2025-11-14 09:40:36 +0500

    Create conftest.py

---
+++ config/conftest.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+ 
+    BASE_API_URL = "https://web-agr.chitai-gorod.ru/web/api/v2"
+    API_TIMEOUT = 30  
+    
+   
+    BASE_UI_URL = "https://www.chitai-gorod.ru"
+    BROWSER = os.getenv("BROWSER", "chrome")
+    HEADLESS = os.getenv("HEADLESS", "False").lower() == "true"
+    IMPLICIT_WAIT = 5  
+    EXPLICIT_WAIT = 20 
+    
+    
+    CUSTOMER_CITY_ID = 213
+    AB_TEST_GROUP = 1
+
+settings = Settings()
