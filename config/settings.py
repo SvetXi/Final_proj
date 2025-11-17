9102a01d4ab990be532f6580d7716fe3d5a9cbb2
SvetXi <svetamisaleva@mail.ru>
2025-11-17 08:50:36 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,14 +6,14 @@ load_dotenv()
 class Settings:
     # API Settings
     BASE_API_URL = "https://web-agr.chitai-gorod.ru/web/api/v2"
-    API_TIMEOUT = 10
+    API_TIMEOUT = 30
     
     # UI Settings
     BASE_UI_URL = "https://www.chitai-gorod.ru"
     BROWSER = os.getenv("BROWSER", "chrome")
     HEADLESS = os.getenv("HEADLESS", "False").lower() == "true"
-    IMPLICIT_WAIT = 10
-    EXPLICIT_WAIT = 15
+    IMPLICIT_WAIT = 5
+    EXPLICIT_WAIT = 20
     
     # Test Data
     CUSTOMER_CITY_ID = 213
