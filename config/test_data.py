e355fc3f22b617df491d08ba8b5fd9da38c9d7ec
SvetXi <svetamisaleva@mail.ru>
2025-11-19 09:05:17 +0500

    Update test_data.py

--- config/test_data.py
+++ config/test_data.py
@@ -2,29 +2,13 @@ class TestData:
     # API Test Data
     SEARCH_PHRASES = {
         "cyrillic": "книга",
-        "latin": "Book", 
-        "special_chars": "!@#$%",
-        "numbers": "123",
-        "existing_product": "Сияние"  
+        "latin": "book", 
+        "special_chars": "!@#$%"
     }
     
-    # UI Test Data 
-    VALID_PHONE = "+79111111111"  
-    SMS_CODE = "000000" 
-    SEARCH_QUERY = "книга" 
-    AUTHOR_FILTER = "Достоевский" 
-    
-    # Expected Messages
-    ERROR_MESSAGES = {
-        "invalid_phrase": "Недопустимая поисковая фраза",
-        "empty_phrase": "Значение не должно быть пустым",
-        "unauthorized": "Authorization обязательное поле"
-    }
-    
-    # URLs 
-    AUTH_URL = "/login"
-    SEARCH_URL = "/search"
-    CART_URL = "/cart"
-    MAIN_URL = "/"
+    # UI Test Data
+    VALID_PHONE = "+79111111111"
+    SEARCH_QUERY = "книга"
+    AUTHOR_FILTER = "книги"
 
 test_data = TestData()
