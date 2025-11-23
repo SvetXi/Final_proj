9edfad8f902984077ccad19c792b2bc5b1813eb6
SvetXi <svetamisaleva@mail.ru>
2025-11-23 09:18:37 +0500

    Update api_client.py

--- utils/api_client.py
+++ utils/api_client.py
@@ -1,11 +1,18 @@
 import requests
 import allure
 from config.settings import settings
+from typing import Optional
 
 class APIClient:
     def __init__(self, base_url: str = settings.BASE_API_URL):
         self.base_url = base_url
         self.session = requests.Session()
+    def init(self, base_url: str, token: Optional[str] = None, timeout: float = 10.0):
+        self.timeout = timeout
+        self.token = token or self._load_token_from_env()
+        if not self.token:
+            raise ValueError("API token must be provided either via parameter or environment variable.")
+        return {"Authorization": f"Bearer {self.token}"} if self.token else {}
     
     @allure.step("Execute facet search")
     def facet_search(self, phrase: str):
