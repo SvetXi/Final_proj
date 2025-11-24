718cc35cee57a3156389d44af62b69744b87f938
SvetXi <svetamisaleva@mail.ru>
2025-11-25 01:59:44 +0500

    Update api_client.py

--- utils/api_client.py
+++ utils/api_client.py
@@ -3,60 +3,10 @@ import allure
 from config.settings import settings
 from typing import Optional
 
-  # Конфигурация
-AUTH_URL = "https://web-agr.chitai-gorod.ru/web/api/v2"  
-CLIENT_ID = "tid_chitaigorod"
-CLIENT_SECRET = "secret"
-
-API_BASE_URL = "https://web-agr.chitai-gorod.ru/web/api/v2"
-
-class TokenManager:
-    def __init__(self, auth_url: str, client_id: str, client_secret: str):
-        self.auth_url = auth_url
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self._token: Optional[str] = None
-        self._expires_at: Optional[float] = None
-
-    def _is_expired(self) -> bool:
-        if self._token is None or self._expires_at is None:
-            return True
-
-    def _fetch_token(self) -> None:
-        data = {
-            "grant_type": "client_credentials"
-        }
-        auth = (self.client_id, self.client_secret)
-
-        resp = requests.post(self.auth_url, data=data, auth=auth)
-        resp.raise_for_status()
-        payload = resp.json()
-
-        # ожидаемые поля: access_token и expires_in (в секундах)
-        access_token = payload.get("access_token")
-        expires_in = payload.get("expires_in", 3600)
-
-        if not access_token:
-            raise RuntimeError(format(payload))
-
-        self._token = access_token
-
-    def get_token(self) -> str:
-        if self._is_expired():
-            self._fetch_token()
-        if not self._token:
-            raise RuntimeError("Token не инициализирован")
-        return self._token
-
 class ApiClient:
-    def __init__(self, base_url: str, token_manager: TokenManager):
-        self.base_url = base_url
-        self.token_manager = token_manager
-        self.session = requests.Session()
 
-    def _authorized_headers(self) -> dict:
-        token = self.token_manager.get_token()
-        return {"Authorization": f"Bearer {token}"}
+    def setup_method(self):
+        self.api_client = ApiClient(base_url="https://web-agr.chitai-gorod.ru/web/api/v2/search/facet-search")
 
     def get(self, path: str, **kwargs):
         url = self.base_url.rstrip("/") + "/" + path.lstrip("/")
